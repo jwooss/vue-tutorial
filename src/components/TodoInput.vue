3a7564b0e40175be5eabaed63a16aa0d92ca5e5b
jwoosss <wodn4131@gmail.com>
2018-11-02 22:17:13 +0900

    기능 추가
    - 애니메이션 추가
    - 모달창 추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -5,14 +5,27 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="body" @close="false"></span>
+      <span slot="footer" @click="showModal = false">
+         할 일을 입력하세요.
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+    </span>
+    </modal>
   </div>
 </template>
 
 <script>
+    import Modal from "./common/Modal";
+
     export default {
+        props: ['propsdata'],
         data() {
             return {
-                newTodoItem: ''
+                newTodoItem: '',
+                showModal: false
             }
         },
         methods: {
@@ -21,29 +34,37 @@
                     let value = this.newTodoItem && this.newTodoItem.trim();
                     this.$emit('addTodo', value);
                     this.clearInput();
+                } else {
+                    this.showModal = !this.showModal;
                 }
             },
             clearInput() {
                 this.newTodoItem = '';
             }
-        }
+        },
+        components: {
+            Modal: Modal
+        },
     }
 </script>
 
 <style scoped>
-  input:focus{
+  input:focus {
     outline: none;
   }
-  .inputBox{
+
+  .inputBox {
     background: white;
     height: 50px;
     line-height: 50px;
     border-radius: 5px;
   }
+
   .inputBox input {
     border-style: none;
     font-size: 0.9rem;
   }
+
   .addContainer {
     float: right;
     background: linear-gradient(to right, #6478FB, #8763FB);
@@ -51,6 +72,7 @@
     width: 3rem;
     border-radius: 0 5px 5px 0;
   }
+
   .addBtn {
     color: white;
     vertical-align: middle;
